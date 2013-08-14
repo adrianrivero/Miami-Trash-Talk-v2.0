bd14091257a5ec957980fb462d18fe973715c732
Adrian Rivero <adrianrivero@fullsail.edu>
2013-08-14 00:15:32 -0400

    Responsive update

--- js/java.js
+++ js/java.js
@@ -1,7 +1,60 @@
-    
-        $(document).ready(function () {
-    $('#wrapper').scrollTo('ITEM-ID-HERE', 0);
-    ......
-    ......
-    ......
+/* If installing in the footer, you won't need $(function() {} */
+
+    $(function(){
+     $('#slider-1').liquidSlider();
+    });
+
+    $(function(){
+     $('#slider-2').liquidSlider();
+    });
+
+    $(function(){
+     $('#slider-3').liquidSlider();
+    });
+
+    $(function(){
+     $('#slider-4').liquidSlider();
+    });
+
+    $(function(){
+     $('#slider-5').liquidSlider();
+    });
+    </script>
+    <script>
+
+    $(document).ready(function() {
+
+    $('a.panel').click(function () {
+
+        $('a.panel').removeClass('selected');
+        $(this).addClass('selected');
+        
+        current = $(this);
+                
+        $('#wrapper').scrollTo($(this).attr('href'), 800);      
+        
+        return false;
+    });
+
+    $(window).resize(function () {
+        resizePanel();
+    });
+
+    });
+
+    function resizePanel() {
+
+    width = $(window).width();
+    height = $(window).height();
+
+    mask_height = height * $('.item').length;
+        
+    $('#debug').html(width  + ' ' + height + ' ' + mask_height);
+
+    $('#wrapper, .item').css({width: width, height: height});
+    $('#mask').css({width: width, height: mask_height});
+    $('#wrapper').scrollTo($('a.selected').attr('href'), 0);
+        
     }
+
+    </script>
